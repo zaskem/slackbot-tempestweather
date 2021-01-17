9422ee2a1f9d73be1df9d32ebb66b4d856563582
Matt Zaske <zaskem@mzonline.com>
2021-01-17 14:28:19 -0600

    Renamed Function File

--- UtilityFunctions.php
+++ UtilityFunctions.php
@@ -20,7 +20,8 @@
   /**
    * getParentKey($needle, $haystack, $searchColumn) - Returns the "outer" (parent) key in which $needle matches a value in $searchColumn
    * 
-   * This is intended to be used on a single-depth multidimensional array...and intended to only find 1 match (one unique value).
+   * This is intended to be used on a single-depth multidimensional array...and intended to only find/return 1 match (one unique value).
+   * For this project, it's generally used to find the parent observation ID for a given measurement such as high temperature or wind speed.
    */
   function getParentKey($needle, $haystack, $searchColumn) {
     foreach($haystack as $key => $value) {
@@ -31,24 +32,20 @@
   }
   
   /**
-   * Functions provided for convenience of converting between units.
+   * Functions provided for converting between station units.
    */
   function convertCToF($cValue = 0) {
     return number_format((($cValue * 9 / 5) + 32), 1);
   }
-
   function convertMPSToMPH($mpsValue) {
     return number_format(($mpsValue * 2.23694), 1);
   }
-
   function convertMbToInHg($mbValue) {
     return number_format(($mbValue / 33.864), 3);
   }
-
   function convertMmToInch($mmValue) {
     return number_format(($mmValue / 25.4), 2);
   }
-
   function convertKmToMiles($kmValue) {
     return number_format(($kmValue / 1.609344497892563), 2);
   }
